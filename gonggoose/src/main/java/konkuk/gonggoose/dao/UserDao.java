8e9a1c5711c53aa10b529407b1f2a34673ed8b7e
Minseok <saltishDev@gmail.com>
2024-07-06 00:09:26 +0900

    Feat: 회원가입

--- gonggoose/src/main/java/konkuk/gonggoose/dao/UserDao.java
+++ gonggoose/src/main/java/konkuk/gonggoose/dao/UserDao.java
@@ -1,11 +1,18 @@
 package konkuk.gonggoose.dao;
 
+import konkuk.gonggoose.dto.SignupRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.time.LocalDate;
 import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 @Repository
@@ -21,5 +28,26 @@ public class UserDao {
         String sql = "select exists(select kakao_id from user where kakao_id=:kakao_id)";
         Map<String, Object> param = Map.of("kakao_id", kakaoId);
         return Boolean.TRUE.equals(jdbcTemplate.queryForObject(sql, param, boolean.class));
+    }
+
+    public long signup(SignupRequest signupRequest, String createdAt, String updatedAt) {
+        log.info("[UserDao.signup]");
+
+        String sql = "insert into user(kakao_id, nickname, image_url, school_name, school_email, token)" +
+                "values(:kakao_id, :nickname, :image_url, :school_name, :school_email, :token)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+//        SqlParameterSource param = new BeanPropertySqlParameterSource(signupRequest);
+        Map<String, Object> param = Map.of( "user_id", Objects.requireNonNull(keyHolder.getKey()).longValue(),
+                "kakao_id", signupRequest.getKakaoId(),
+                "nickname", signupRequest.getNickname(),
+                "image_url", signupRequest.getImageUrl(),
+                "school_name", signupRequest.getSchoolName(),
+                "school_email", signupRequest.getSchoolEmail(),
+                "token", signupRequest.getAccessToken(),
+                "created_at", createdAt,
+                "updated_at", updatedAt);
+        jdbcTemplate.update(sql, param);
+        return Objects.requireNonNull(keyHolder.getKey()).longValue();
     }
 }
--- gonggoose/src/main/java/konkuk/gonggoose/service/UserService.java
+++ gonggoose/src/main/java/konkuk/gonggoose/service/UserService.java
@@ -1,9 +1,13 @@
 package konkuk.gonggoose.service;
 
 import konkuk.gonggoose.dao.UserDao;
+import konkuk.gonggoose.dto.SignupRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Slf4j
 @Service
@@ -14,5 +18,12 @@ public class UserService {
 
     public boolean isExistedUser(long kakaoId) {
         return userDao.isExistedUser(kakaoId);
+    }
+
+    public void signup(SignupRequest signupRequest) {
+        String createdAt = LocalDate.now().toString();
+        String updatedAt = "";
+
+        userDao.signup(signupRequest, createdAt, updatedAt);
     }
 }
