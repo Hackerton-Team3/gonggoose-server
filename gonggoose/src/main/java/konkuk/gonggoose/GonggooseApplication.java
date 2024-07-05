0582705fc41e2e96cb6d9c197c4cfd59297f1db1
곽유나 <yoona405@konkuk.ac.kr>
2024-07-05 14:50:19 +0900

    초기 설정

---
+++ gonggoose/src/main/java/konkuk/gonggoose/GonggooseApplication.java
@@ -0,0 +1,13 @@
+package konkuk.gonggoose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonggooseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonggooseApplication.class, args);
+	}
+
+}
---
+++ gonggoose/src/test/java/konkuk/gonggoose/GonggooseApplicationTests.java
@@ -0,0 +1,13 @@
+package konkuk.gonggoose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonggooseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
